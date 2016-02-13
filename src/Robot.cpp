67297e1ecd690060426a074cde6428c5bcdbf5a8
readerboy007 <readerboy007@embarqmail.com>
2016-02-12 19:50:36 -0600

    Added shooter, changed pneumatics

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,6 +1,7 @@
 #include "WPILib.h"
 //#include <iostream>
 
+
 class Robot: public IterativeRobot
 {
 	CANTalon *lDrive1;
@@ -8,11 +9,11 @@ class Robot: public IterativeRobot
 	CANTalon *rDrive1;
 	CANTalon *rDrive2;
 	CANTalon *arm1;
-	CANTalon *arm2;
+	CANTalon *shooter;
 	//CANTalon *flipper1;
 	//CANTalon *flipper2;
-	DoubleSolenoid *lShifter;
-	DoubleSolenoid *rShifter;
+	DoubleSolenoid *shifter;
+	DoubleSolenoid *william;
 	DoubleSolenoid *flipper1;
 	DoubleSolenoid *flipper2;
 	Joystick *driveStick;
@@ -30,18 +31,18 @@ public:
 		rDrive1 = new CANTalon(3);
 		rDrive2 = new CANTalon(4);
 		arm1 = new CANTalon(5);
-		arm2 = new CANTalon(6);
+		shooter = new CANTalon(6);
 		//flipper1 = new CANTalon(7);
 		//flipper2 = new CANTalon(8);
-		lShifter = new DoubleSolenoid(0,1);
-		rShifter = new DoubleSolenoid(2,3);
+		shifter = new DoubleSolenoid(0,1);
+		william = new DoubleSolenoid(2,3);
 		flipper1 = new DoubleSolenoid(4,5);
 		flipper2 = new DoubleSolenoid(6,7);
 		driveStick = new Joystick(0);
 		manipulatorStick = new Joystick(1);
 		steven = new Compressor(0);
 
-		CameraServer::GetInstance()->SetQuality(50);
+		//CameraServer::GetInstance()->SetQuality(50);
 		//table = NetworkTable::GetTable("GRIP/myContoursReport");
 	}
 
@@ -60,7 +61,8 @@ private:
 	double mRightY = 0;
 	double leftTrigger = 0;
 	double rightTrigger = 0;
-	double threshold = 0.1;
+	double mRightTrigger = 0;
+	double threshold = 0.09;
 
 	void RobotInit() override
 	{
@@ -105,9 +107,9 @@ private:
 	{
 		if(autoSelected == autoNameCustom){
 			lDrive1->Set(.475);
-			rDrive1->Set(.475);
+			rDrive1->Set(-.475);
 			lDrive2->Set(.475);
-			rDrive2->Set(.475);
+			rDrive2->Set(-.475);
 			Wait(4);
 			lDrive1->Set(0);
 			rDrive1->Set(0);
@@ -125,8 +127,8 @@ private:
 
 	void TeleopPeriodic()
 	{
-		CameraServer::GetInstance()->StartAutomaticCapture("cam0");
-		steven->SetClosedLoopControl(true);
+		//CameraServer::GetInstance()->StartAutomaticCapture("cam0");
+		//steven->SetClosedLoopControl(true);
 
 		leftX = driveStick->GetRawAxis(0);
 		if(fabs(leftX) < threshold)
@@ -152,43 +154,67 @@ private:
 		rightTrigger = (driveStick->GetRawAxis(3));
 		if(fabs(rightTrigger) < (threshold))
 			rightTrigger = 0;
+		mRightTrigger = (driveStick->GetRawAxis(3));
+		if(fabs(mRightTrigger) < (threshold))
+			mRightTrigger = 0;
 
-		leftX = pow(leftX, 3.0);
-		leftY = pow(leftY, 3.0);
-		rightX = pow(rightX, 3.0);
-		rightY = pow(rightY, 3.0);
-		mRightY = pow(mRightY, 3.0);
-		mLeftY = pow(mLeftY, 3.0);
+		leftX = pow(fabs(leftX), 1.4);
+		if(driveStick->GetRawAxis(0) > 0) {
+			leftX = -leftX;
+		}
+		/*if(driveStick->GetRawAxis(1) < 0) {
+			leftY = -leftY;
+		}*/
+		leftY = pow(fabs(leftY), 1.4);
+		if(driveStick->GetRawAxis(1) < 0) {
+			leftY = -leftY;
+		}
+		rightX = pow(fabs(rightX), 1.4);
+		if(driveStick->GetRawAxis(4) > 0) {
+			rightX = -rightX;
+		}
+		rightY = pow(fabs(rightY), 1.4);
+		if(driveStick->GetRawAxis(5) > 0) {
+			rightY = -rightY;
+		}
+		mRightY = pow(fabs(mRightY), 1.4);
+		if(manipulatorStick->GetRawAxis(5) > 0) {
+			mRightY = -mRightY;
+		}
+		mLeftY = pow(fabs(mLeftY), 1.4);
+		if(manipulatorStick->GetRawAxis(1) > 0) {
+			mLeftY = -mLeftY;
+		}
 
-		rDrive1->Set(-(leftY + leftX));
-		rDrive2->Set(-(leftY + leftX));
-		lDrive1->Set(leftY - leftX);
-		lDrive2->Set(leftY - leftX);
+		rDrive1->Set(-(leftY - leftX));
+		rDrive2->Set(-(leftY - leftX));
+		lDrive1->Set(leftY + leftX);
+		lDrive2->Set(leftY + leftX);
 
 		if(driveStick->GetRawButton(3)) {
-			lShifter->Set(DoubleSolenoid::Value::kForward);
-			rShifter->Set(DoubleSolenoid::Value::kForward);
+			shifter->Set(DoubleSolenoid::Value::kForward);
+			//rShifter->Set(DoubleSolenoid::Value::kForward);
 		} else if(driveStick->GetRawButton(2)) {
-			lShifter->Set(DoubleSolenoid::Value::kReverse);
-			rShifter->Set(DoubleSolenoid::Value::kReverse);
+			shifter->Set(DoubleSolenoid::Value::kReverse);
+			//rShifter->Set(DoubleSolenoid::Value::kReverse);
 		}
 
 
 
-		if(manipulatorStick->GetRawButton(6)) {
+		if(manipulatorStick->GetRawButton(5)) {
 			arm1->Set(mRightY);
-		} else if(driveStick->GetRawButton(6)) {
+		} else if(driveStick->GetRawButton(5)) {
 			arm1->Set(rightY);
 		} else {
-			arm1 = 0;
+			arm1->Set(0);
 		}
-		if(manipulatorStick->GetRawButton(5)) {
+/*		if(manipulatorStick->GetRawButton(5)) {
 			arm2->Set(mLeftY);
 		} else if(driveStick->GetRawButton(5)) {
 			arm2->Set(rightY);
 		} else {
-			arm2 = 0;
-		}
+			arm2->Set(0);
+		}*/
 
 		/*if(!manipulatorStick->GetRawButton(6) && !manipulatorStick->GetRawButton(5)) {
 			flipper1->Set(mRightY);
@@ -211,6 +237,18 @@ private:
 		} else if(manipulatorStick->GetRawButton(4) || driveStick->GetRawButton(4)) {
 			flipper1->Set(DoubleSolenoid::Value::kReverse);
 			flipper2->Set(DoubleSolenoid::Value::kReverse);
+		}
+
+		if(fabs(mRightTrigger) > threshold) {
+			shooter->Set(mRightTrigger);
+		} else {
+			shooter->Set(0);
+		}
+
+		if(manipulatorStick->GetRawButton(6)) {
+			william->Set(DoubleSolenoid::Value::kForward);
+		} else {
+			william->Set(DoubleSolenoid::Value::kReverse);
 		}
 
 	}//:D
