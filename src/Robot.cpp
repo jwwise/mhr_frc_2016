5005929764d2abfeaaff3d7c942b1e0638341980
DriveStation <drivestation@metrohomeschoolrobotics.org>
2016-03-10 16:56:43 -0600

    Backup

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,6 +1,4 @@
 #include "WPILib.h"
-#include <vector>
-#include <cmath>
 //#include <iostream>
 
 
@@ -29,12 +27,6 @@ class Robot: public IterativeRobot
 	DigitalInput *switch3;
 	DigitalInput *switch4;
 	AnalogInput *sonic;
-
-	//Images
-	Image *frame;
-	Image *binaryFrame;
-	IMAQdxSession session;
-
 	//I2C *arduino;
 	/*DigitalOutput *sonicOut;
 	PWM *sonicIn;*/
@@ -70,8 +62,6 @@ public:
 		switch3 = new DigitalInput(2);
 		switch4 = new DigitalInput(3);
 		sonic = new AnalogInput(0);
-		/*frame = new Image(0);
-		binaryFrame = new Image(1);*/
 		//arduino = new I2C(4);
 		//sonicOut = new DigitalOutput(4);
 		//sonicIn = new PWM(0);
@@ -119,39 +109,7 @@ private:
 	int mode = 0;
 	double fieldPos = 0;
 	double threshold = 0.09;
-	double shooterPos = 0;
 	double speed = 0;
-
-	int imaqError;
-
-	//A structure to hold measurements of a particle
-		struct ParticleReport {
-			double PercentAreaToImageArea;
-			double Area;
-			double BoundingRectLeft;
-			double BoundingRectTop;
-			double BoundingRectRight;
-			double BoundingRectBottom;
-		};
-
-		//Structure to represent the scores for the various tests used for target identification
-		struct Scores {
-			double Area;
-			double Aspect;
-		};
-
-		//Constants
-		Range RING_HUE_RANGE = {101, 64};	//Default hue range for ring light
-		Range RING_SAT_RANGE = {88, 255};	//Default saturation range for ring light
-		Range RING_VAL_RANGE = {134, 255};	//Default value range for ring light
-		double AREA_MINIMUM = 0.5; //Default Area minimum for particle as a percentage of total image area
-		double LONG_RATIO = 2.22; //long side = 26.9 / height = 12.1 = 2.22
-		double SHORT_RATIO = 1.4; //short side = 16.9 / height = 12.1 = 1.4
-		double SCORE_MIN = 75.0;  //Minimum score to be considered a tote
-		double VIEW_ANGLE = 48.5; //View angle fo camera, set to Axis m1011 by default, 64 for m1013, 51.7 for 206, 52 for HD3000 square, 60 for HD3000 640x480
-		ParticleFilterCriteria2 criteria[1];
-		ParticleFilterOptions2 filterOptions = {0,0,1,1};
-		Scores scores;
 	//double gearRatio = 18.75;
 	//double gearRatio = 15.32;
 	/*double distance = 0;
@@ -174,22 +132,11 @@ private:
 
 	void RobotInit() override
 	{
-		// create images
-		frame = imaqCreateImage(IMAQ_IMAGE_RGB, 0);
-		binaryFrame = imaqCreateImage(IMAQ_IMAGE_U8, 0);
-
-		imaqError = IMAQdxOpenCamera("cam0", IMAQdxCameraControlModeController, &session);
-		if(imaqError != IMAQdxErrorSuccess) {
-			DriverStation::ReportError("IMAQdxOpenCamera error: " + std::to_string((long)imaqError) + "\n");
-		}
-		imaqError = IMAQdxConfigureGrab(session);
-		if(imaqError != IMAQdxErrorSuccess) {
-			DriverStation::ReportError("IMAQdxConfigureGrab error: " + std::to_string((long)imaqError) + "\n");
-		}
-
 		/*frame = imaqCreateImage(IMAQ_IMAGE_RGB, 0);
 		imaqColorThreshold(frame2, frame, 0, 0, Range(0), Range(255), Range(0);*/
 
+		//CameraServer::GetInstance()->SetQuality(50);
+		//CameraServer::GetInstance()->StartAutomaticCapture("cam0");
 
 		/*chooser = new SendableChooser();
 		chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
@@ -346,8 +293,6 @@ private:
 			rDrive2->Set(0);
 			flipper->Set(DoubleSolenoid::Value::kReverse);
 		}
-
-
 
 
 		/*autoSelected = *((std::string*)chooser->GetSelected());
@@ -607,62 +552,57 @@ private:
 		if(manualShoot) {
 			if(driveStick->GetRawButton(8)) {
 				if(fabs(rightTrigger)> threshold) {
-					shooter1->Set(-rightTrigger / 2.5);
+					shooter1->Set(rightTrigger / 2.5);
 				} else if(fabs(mRightTrigger) > threshold) {
-					shooter1->Set(-mRightTrigger / 2.5);
+					shooter1->Set(mRightTrigger / 2.5);
 				} else {
 					shooter1->Set(0);
 				}
 			} else {
 				if(fabs(rightTrigger)> threshold) {
-					shooter1->Set(rightTrigger / 1.2);
+					shooter1->Set(-rightTrigger / 1.2);
 				} else if(fabs(mRightTrigger) > threshold) {
-					shooter1->Set(mRightTrigger / 1.2);
+					shooter1->Set(-mRightTrigger / 1.2);
 				} else {
 					shooter1->Set(0);
 				}
 			}
 			if(driveStick->GetRawButton(7)) {
 				if(fabs(leftTrigger)> threshold) {
-					shooter2->Set(leftTrigger / 2.5);
+					shooter2->Set(-leftTrigger / 2.5);
 				} else if(fabs(mLeftTrigger) > threshold) {
-					shooter2->Set(mLeftTrigger / 2.5);
+					shooter2->Set(-mLeftTrigger / 2.5);
 				} else {
 					shooter2->Set(0);
 				}
 			} else {
 				if(fabs(leftTrigger)> threshold) {
-					shooter2->Set(-leftTrigger / 1.2);
+					shooter2->Set(leftTrigger / 1.2);
 				} else if(fabs(mLeftTrigger) > threshold) {
-					shooter2->Set(-mLeftTrigger / 1.2);
+					shooter2->Set(mLeftTrigger / 1.2);
 				} else {
 					shooter2->Set(0);
 				}
 			}
 		} else {
 			if(fabs(rightTrigger)> threshold) {
-				shooter1->Set(rightTrigger / 1.2);
-				shooter2->Set(-rightTrigger / 1.2);
+				shooter1->Set(-rightTrigger / 1.2);
+				shooter2->Set(rightTrigger / 1.2);
 			} else if(fabs(mRightTrigger) > threshold) {
-				shooter1->Set(mRightTrigger / 1.2);
-				shooter2->Set(-mRightTrigger / 1.2);
+				shooter1->Set(-mRightTrigger / 1.2);
+				shooter2->Set(mRightTrigger / 1.2);
 			} else if(fabs(leftTrigger)> threshold) {
-				shooter1->Set(-leftTrigger / 2.5);
-				shooter2->Set(leftTrigger / 2.5);
+				shooter1->Set(leftTrigger / 2.5);
+				shooter2->Set(-leftTrigger / 2.5);
 			} else if(fabs(mLeftTrigger) > threshold) {
-				shooter1->Set(-mLeftTrigger / 2.5);
-				shooter2->Set(mLeftTrigger / 2.5);
+				shooter1->Set(mLeftTrigger / 2.5);
+				shooter2->Set(-mLeftTrigger / 2.5);
 			} else {
 				shooter1->Set(0);
 				shooter2->Set(0);
 			}
 		}
 
-		if(switch1->Get() == false) {
-			shooterPos = 0;
-		} else {
-			shooterPos = shooterPos - winch->Get();
-		}
 
 		if(driveStick->GetRawButton(5)) {
 			william->Set(DoubleSolenoid::Value::kReverse);
@@ -694,18 +634,18 @@ private:
 			winch->Set(-0.5);
 		} else */
 		if(dUp && switch2->Get()) {
-			winch->Set(-0.5);
+			winch->Set(0.5);
 		} else if(dDown && switch1->Get()) {
-			winch->Set(0.5);
-		} else if((manipulatorStick->GetPOV() == 0) && switch2->Get()) {
 			winch->Set(-0.5);
-		} else if((manipulatorStick->GetPOV() == 180) && switch1->Get()) {
+		} else if((manipulatorStick->GetPOV() == 0) && switch2->Get()) {
 			winch->Set(0.5);
+		} else if((manipulatorStick->GetPOV() == 180) && switch1->Get()) {
+			winch->Set(-0.5);
 		} else if(fabs(mLeftY) > threshold) {
 			if(mLeftY > threshold && switch3->Get()) {
-				winch->Set(-mLeftY);
+				winch->Set(mLeftY);
 			} else if(mLeftY < threshold && switch4->Get()) {
-				winch->Set(-mLeftY);
+				winch->Set(mLeftY);
 			}
 		/*} else if(cruise && switch2->Get()) {
 			winch->Set(-0.05);*/
@@ -713,83 +653,14 @@ private:
 			winch->Set(0);
 		}
 
-		if(sonic->GetVoltage() < 0.1) {
+		if(sonic->GetVoltage() < 0.12) {
 			SmartDashboard::PutString("DB/String 9", "Loaded");
 		} else {
 			SmartDashboard::PutString("DB/String 9", "Feed me, please!");
 		}
 
 		SmartDashboard::PutNumber("DB/Slider 1", sonic->GetVoltage());
-		SmartDashboard::PutNumber("DB/Slider 2", shooterPos);
 	}//:D
-
-	void TestInit()
-	{
-		IMAQdxStartAcquisition(session);
-		//read file in from disk. For this example to run you need to copy image.jpg from the SampleImages folder to the
-							//directory shown below using FTP or SFTP: http://wpilib.screenstepslive.com/s/4485/m/24166/l/282299-roborio-ftp
-							//imaqError = imaqReadFile(frame, "//home//lvuser//SampleImages//image.jpg", NULL, NULL);
-
-							//Update threshold values from SmartDashboard. For performance reasons it is recommended to remove this after calibration is finished.
-							/*RING_HUE_RANGE.minValue = SmartDashboard::GetNumber("Tote hue min", RING_HUE_RANGE.minValue);
-							RING_HUE_RANGE.maxValue = SmartDashboard::GetNumber("Tote hue max", RING_HUE_RANGE.maxValue);
-							RING_SAT_RANGE.minValue = SmartDashboard::GetNumber("Tote sat min", RING_SAT_RANGE.minValue);
-							RING_SAT_RANGE.maxValue = SmartDashboard::GetNumber("Tote sat max", RING_SAT_RANGE.maxValue);
-							RING_VAL_RANGE.minValue = SmartDashboard::GetNumber("Tote val min", RING_VAL_RANGE.minValue);
-							RING_VAL_RANGE.maxValue = SmartDashboard::GetNumber("Tote val max", RING_VAL_RANGE.maxValue);*/
-
-							//Threshold the image looking for ring light color
-							imaqError = imaqColorThreshold(binaryFrame, frame, 255, IMAQ_HSV, &RING_HUE_RANGE, &RING_SAT_RANGE, &RING_VAL_RANGE);
-
-							//Send particle count to dashboard
-							int numParticles = 0;
-							imaqError = imaqCountParticles(binaryFrame, 1, &numParticles);
-							SmartDashboard::PutNumber("Masked particles", numParticles);
-
-							//Send masked image to dashboard to assist in tweaking mask.
-							SendToDashboard(binaryFrame, imaqError);
-
-							//filter out small particles
-							float areaMin = SmartDashboard::GetNumber("Area min %", AREA_MINIMUM);
-							criteria[0] = {IMAQ_MT_AREA_BY_IMAGE_AREA, areaMin, 100, false, false};
-							imaqError = imaqParticleFilter4(binaryFrame, binaryFrame, criteria, 1, &filterOptions, NULL, NULL);
-
-							//Send particle count after filtering to dashboard
-							imaqError = imaqCountParticles(binaryFrame, 1, &numParticles);
-							SmartDashboard::PutNumber("Filtered particles", numParticles);
-
-							if(numParticles > 0) {
-								//Measure particles and sort by particle size
-								std::vector<ParticleReport> particles;
-								for(int particleIndex = 0; particleIndex < numParticles; particleIndex++)
-								{
-									ParticleReport par;
-									imaqMeasureParticle(binaryFrame, particleIndex, 0, IMAQ_MT_AREA_BY_IMAGE_AREA, &(par.PercentAreaToImageArea));
-									imaqMeasureParticle(binaryFrame, particleIndex, 0, IMAQ_MT_AREA, &(par.Area));
-									imaqMeasureParticle(binaryFrame, particleIndex, 0, IMAQ_MT_BOUNDING_RECT_TOP, &(par.BoundingRectTop));
-									imaqMeasureParticle(binaryFrame, particleIndex, 0, IMAQ_MT_BOUNDING_RECT_LEFT, &(par.BoundingRectLeft));
-									imaqMeasureParticle(binaryFrame, particleIndex, 0, IMAQ_MT_BOUNDING_RECT_BOTTOM, &(par.BoundingRectBottom));
-									imaqMeasureParticle(binaryFrame, particleIndex, 0, IMAQ_MT_BOUNDING_RECT_RIGHT, &(par.BoundingRectRight));
-									particles.push_back(par);
-								}
-								sort(particles.begin(), particles.end(), CompareParticleSizes);
-
-								//This example only scores the largest particle. Extending to score all particles and choosing the desired one is left as an exercise
-								//for the reader. Note that this scores and reports information about a single particle (single L shaped target). To get accurate information
-								//about the location of the tote (not just the distance) you will need to correlate two adjacent targets in order to find the true center of the tote.
-								scores.Aspect = AspectScore(particles.at(0));
-								SmartDashboard::PutNumber("Aspect", scores.Aspect);
-								scores.Area = AreaScore(particles.at(0));
-								SmartDashboard::PutNumber("Area", scores.Area);
-								bool isTarget = scores.Area > SCORE_MIN && scores.Aspect > SCORE_MIN;
-
-								//Send distance and tote status to dashboard. The bounding rect, particularly the horizontal center (left - right) may be useful for rotating/driving towards a tote
-								SmartDashboard::PutBoolean("IsTarget", isTarget);
-								SmartDashboard::PutNumber("Distance", computeDistance(binaryFrame, particles.at(0)));
-							} else {
-								SmartDashboard::PutBoolean("IsTarget", false);
-							}
-	}
 
 	void TestPeriodic()
 	{
@@ -817,7 +688,7 @@ private:
 		SmartDashboard::PutString("DB/String 6", "Position: Slider 0");
 		fieldPos = SmartDashboard::GetNumber("DB/Slider 0", 0.0);
 
-		if(sonic->GetVoltage() < 0.1) {
+		if(sonic->GetVoltage() < 0.13) {
 			SmartDashboard::PutString("DB/String 9", "Loaded");
 		} else {
 			SmartDashboard::PutString("DB/String 9", "Feed me, please!");
@@ -1009,71 +880,6 @@ private:
 			}
 		}*/
 	}
-
-	//Send image to dashboard if IMAQ has not thrown an error
-		void SendToDashboard(Image *image, int error)
-		{
-			if(error < ERR_SUCCESS) {
-				DriverStation::ReportError("Send To Dashboard error: " + std::to_string((long)imaqError) + "\n");
-			} else {
-				CameraServer::GetInstance()->SetImage(binaryFrame);
-			}
-		}
-
-		//Comparator function for sorting particles. Returns true if particle 1 is larger
-		static bool CompareParticleSizes(ParticleReport particle1, ParticleReport particle2)
-		{
-			//we want descending sort order
-			return particle1.PercentAreaToImageArea > particle2.PercentAreaToImageArea;
-		}
-
-		/**
-		 * Converts a ratio with ideal value of 1 to a score. The resulting function is piecewise
-		 * linear going from (0,0) to (1,100) to (2,0) and is 0 for all inputs outside the range 0-2
-		 */
-		double ratioToScore(double ratio)
-		{
-			return (fmax(0, fmin(100*(1-fabs(1-ratio)), 100)));
-		}
-
-
-		double AreaScore(ParticleReport report)
-		{
-			double boundingArea = (report.BoundingRectBottom - report.BoundingRectTop) * (report.BoundingRectRight - report.BoundingRectLeft);
-			//Tape is 7" edge so 49" bounding rect. With 2" wide tape it covers 24" of the rect.
-			return ratioToScore((49/24)*report.Area/boundingArea);
-		}
-
-		/**
-		 * Method to score if the aspect ratio of the particle appears to match the retro-reflective target. Target is 7"x7" so aspect should be 1
-		 */
-		double AspectScore(ParticleReport report)
-		{
-			return ratioToScore(((report.BoundingRectRight-report.BoundingRectLeft)/(report.BoundingRectBottom-report.BoundingRectTop)));
-		}
-
-
-		/**
-		 * Computes the estimated distance to a target using the width of the particle in the image. For more information and graphics
-		 * showing the math behind this approach see the Vision Processing section of the ScreenStepsLive documentation.
-		 *
-		 * @param image The image to use for measuring the particle estimated rectangle
-		 * @param report The Particle Analysis Report for the particle
-		 * @return The estimated distance to the target in feet.
-		 */
-		double computeDistance (Image *image, ParticleReport report) {
-			double normalizedWidth, targetWidth;
-			int xRes, yRes;
-
-			imaqGetImageSize(image, &xRes, &yRes);
-			normalizedWidth = 2*(report.BoundingRectRight - report.BoundingRectLeft)/xRes;
-			SmartDashboard::PutNumber("Width", normalizedWidth);
-			targetWidth = 7;
-
-			return  targetWidth/(normalizedWidth*12*tan(VIEW_ANGLE*M_PI/(180*2)));
-		}
 };
 
 START_ROBOT_CLASS(Robot)
-
-//lol
